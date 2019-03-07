2d4d6af028324abc4d1edb6af420751f796b485b
Nelli <nelli.vassila@nortal.com>
2019-03-07 22:57:02 +0200

    Refactoring Unit test

--- tests/ReceiptTest.php
+++ tests/ReceiptTest.php
@@ -6,12 +6,23 @@ use PHPUnit\Framework\TestCase;
 use TDD\Receipt;
 
 class ReceiptTest extends TestCase {
+    // Lisan uue funktsiooni setUp
+    public function setUp() {
+        $this->Receipt = new Receipt();
+    }
+
+    // Lisan funktsiooni tearDown, et lasta välja testi eraldi
+    public function tearDown() {
+        unset($this->Receipt);
+    }
+
     //Test, mis kontrollib, et kviitungi summa oleks 15
     public function testTotal() {
-        $Receipt = new Receipt();
+        $input = [0,2,5,8];
+        $output = $this->Receipt->total($input);
         $this->assertEquals(
             15,
-            $Receipt->total([0,2,5,8]),
+            $output,
             'When summing the total should equal 15'
         );
     }
